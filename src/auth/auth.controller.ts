0f36c804eee18b2a8a1cff5764f1a0f923b7dde7
hamidraza1 <hrazaa306@gmail.com>
2023-06-27 19:09:15 +0200

    use pipes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  private signup(@Body() dto: any) {
+  private signup(@Body() dto: AuthDto) {
     console.log('inside');
     console.log(dto);
     return 'i am signed up.';
